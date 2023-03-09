aad0a7041a39ae2c022819f98c25aa8a3407d371
Your Name <ttizze@gmail.com>
2023-03-09 21:39:16 +0900

    basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/tweetAnalyzeAI'
 }
 
-module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '/tweetAnalyzeAI',
-}
