d1453ed6ab33faa130796b4f6670e8571c9cbae0
tizze <tttizze@gmail.com>
2023-03-09 22:08:47 +0900

    h

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+const dir = String(process.env.NODE_ENV)
 
 const inter = Inter({ subsets: ['latin'] })
 
